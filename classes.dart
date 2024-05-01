50444062cfe87d06259a383e392c29dd02b32890
Mdednnjya <117470340+Mdednnjya@users.noreply.github.com>
2024-05-02 00:31:01 +0700

    Adding some function
    Adding toString and implement overriding

--- classes.dart
+++ classes.dart
@@ -3,18 +3,23 @@ void main () {
   var taco = MenuItem("Original Taco", 1.9);
   var pizza = Pizza({"Papper", "Mushroom"}, "Limo Pizza", 3.99);
 
-  print(taco.format());
-  print(pizza.format());
+  print(taco);
+  print(pizza);
 }
 
 class MenuItem { // Init a class
   String title; 
   double price;
 
-  MenuItem(this.title, this.price); // connected atributs with "this.atribut" through parameter
+  MenuItem(this.title, this.price); // connected atributs with "this.atribut"
 
   String format() {
     return "$title price is $price";
+  }
+
+  @override
+  String toString() {
+    return format();
   }
 
   }
@@ -23,5 +28,21 @@ class MenuItem { // Init a class
     Set<String> toppings; // Extended atribut
 
     Pizza(this.toppings, super.title, super.price);
+
+    @override // Explicitly write an overide function
+    String format() {
+      var formattedToppings = "Contains:";
+
+      for (final t in toppings) {
+        formattedToppings = "$formattedToppings $t";
+      }
+
+      return "$title price is $price\n$formattedToppings";
+    }
+
+    @override
+    String toString() {
+      return "Instance of pizza: $title, $price, $toppings";
+    }
   }
 
